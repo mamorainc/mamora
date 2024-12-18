fe117b3b5227f0e5f0c47988f530543432434d3b
iamnas <iamnas.eth@gmail.com>
2024-12-19 01:17:19 +0530

    Resolved merge conflicts

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: "Hello welcome to mamora world!",
+  });
+});
+
 app.listen(9000, () => {
   console.log(`app listening on http://localhost:9000`);
 });
