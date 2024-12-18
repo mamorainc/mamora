0ddd4ecbcfba07bc525b8a0127ba0e90dcd6440d
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-12-19 00:57:48 +0530

    setup basinc vscode rules and lint server code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
-import express from 'express'
 import cors from 'cors';
+import express from 'express';
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(9000,()=>{
-    console.log(`app listening on http://localhost:9000`);  
-})
+app.listen(9000, () => {
+  console.log(`app listening on http://localhost:9000`);
+});
