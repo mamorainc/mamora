1551541699afebc2ba2c9ba6adb36fd78fc0ea86
anurag-kochar <anuragmarketing101@gmail.com>
2024-12-21 02:48:08 +0530

    feat: /login page with form

--- frontend/src/app/(auth)/signup/page.tsx
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -1,29 +1,87 @@
-import { Card } from "@/components/ui/card";
 import Link from "next/link";
 import { Logo } from "@/components/logo";
 import { SignUpForm } from "@/components/forms/sign-up";
 
 export default function SignUp() {
   return (
-    <div className="container grid h-svh flex-col items-center justify-center lg:max-w-none lg:px-0">
-      <div className="mx-auto flex w-full flex-col justify-center space-y-2 p-6 sm:w-[480px] lg:p-8">
-        <div className="mb-4 flex items-center justify-center">
+    // <div className="container grid h-svh flex-col items-center justify-center lg:max-w-none lg:px-0">
+    //   <div className="mx-auto flex w-full flex-col justify-center space-y-2 p-6 sm:w-[480px] lg:p-8">
+    //     <div className="mb-4 flex items-center justify-center">
+    //       <Logo />
+    //     </div>
+
+    //     <Card className="p-6">
+    //       <div className="mb-2 flex flex-col space-y-2 text-left">
+    //         <h1 className="text-lg font-semibold tracking-tight">
+    //           Create an account
+    //         </h1>
+    //         <p className="text-sm text-muted-foreground">
+    //           Enter your email and password to create an account. <br />
+    //           Already have an account?{" "}
+    //           <Link
+    //             href="/login"
+    //             className="underline underline-offset-4 hover:text-primary"
+    //           >
+    //             log In
+    //           </Link>
+    //         </p>
+    //       </div>
+    //       <SignUpForm />
+    //       <p className="mt-4 px-8 text-center text-sm text-muted-foreground">
+    //         By creating an account, you agree to our{" "}
+    //         <a
+    //           href="#"
+    //           className="underline underline-offset-4 hover:text-primary"
+    //         >
+    //           Terms of Service
+    //         </a>{" "}
+    //         and{" "}
+    //         <a
+    //           href="#"
+    //           className="underline underline-offset-4 hover:text-primary"
+    //         >
+    //           Privacy Policy
+    //         </a>
+    //         .
+    //       </p>
+    //     </Card>
+    //   </div>
+    // </div>
+
+    <div className="container relative mx-auto grid h-svh flex-col items-center justify-center px-4 lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        <div className="absolute inset-0 bg-zinc-900" />
+        <div className="relative z-20 flex items-center text-lg font-medium">
           <Logo />
         </div>
 
-        <Card className="p-6">
-          <div className="mb-2 flex flex-col space-y-2 text-left">
-            <h1 className="text-lg font-semibold tracking-tight">
+        <div className="relative z-20 mt-auto">
+          <blockquote className="space-y-2">
+            <p className="text-lg">
+              Law Copilot cut our research time in half and improved our case
+              analysis accuracy by 40%. It&apos;s like having an entire legal
+              research team at your fingertips
+            </p>
+            <footer className="text-sm">~ Sarah Chen</footer>
+          </blockquote>
+        </div>
+      </div>
+
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-left">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              {" "}
               Create an account
             </h1>
             <p className="text-sm text-muted-foreground">
               Enter your email and password to create an account. <br />
               Already have an account?{" "}
               <Link
-                href="/signin"
+                href="/login"
                 className="underline underline-offset-4 hover:text-primary"
               >
-                Sign In
+                log In
               </Link>
             </p>
           </div>
@@ -31,21 +89,21 @@ export default function SignUp() {
           <p className="mt-4 px-8 text-center text-sm text-muted-foreground">
             By creating an account, you agree to our{" "}
             <a
-              href="/terms"
+              href="#"
               className="underline underline-offset-4 hover:text-primary"
             >
               Terms of Service
             </a>{" "}
             and{" "}
             <a
-              href="/privacy"
+              href="#"
               className="underline underline-offset-4 hover:text-primary"
             >
               Privacy Policy
             </a>
             .
           </p>
-        </Card>
+        </div>
       </div>
     </div>
   );
