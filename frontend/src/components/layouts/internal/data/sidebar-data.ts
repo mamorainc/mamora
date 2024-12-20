b8c2abd22a024603c3487fe5567be07dda49dc89
anurag-kochar <anuragmarketing101@gmail.com>
2024-12-20 21:12:11 +0530

    add: new internal dashboard sidebar data

--- frontend/src/components/layouts/internal/data/sidebar-data.ts
+++ frontend/src/components/layouts/internal/data/sidebar-data.ts
@@ -51,92 +51,42 @@ export const sidebarData: SidebarData = {
             items: [
                 {
                     title: 'Dashboard',
-                    url: '/',
+                    url: '/dashboard',
                     icon: IconLayoutDashboard,
-                },
-                {
-                    title: 'Tasks',
-                    url: '/tasks',
-                    icon: IconChecklist,
-                },
-                {
-                    title: 'Apps',
-                    url: '/apps',
-                    icon: IconPackages,
-                },
-                {
-                    title: 'Chats',
-                    url: '/chats',
-                    badge: '3',
-                    icon: IconMessages,
-                },
-                {
-                    title: 'Users',
-                    url: '/users',
-                    icon: IconUsers,
-                },
+                }
+
             ],
         },
         {
-            title: 'Pages',
+            title: 'Main',
             items: [
                 {
-                    title: 'Auth',
+                    title: 'Chats',
                     icon: IconLockAccess,
                     items: [
                         {
-                            title: 'Sign In',
-                            url: '/sign-in',
+                            title: 'Create new',
+                            url: '/chat',
                         },
                         {
-                            title: 'Sign In (2 Col)',
-                            url: '/sign-in-2',
+                            title: 'Sending SOL to gupta ji',
+                            url: '/chat/1',
                         },
                         {
-                            title: 'Sign Up',
-                            url: '/sign-up',
+                            title: 'Sending SOL to sharma ji',
+                            url: '/chat/2',
                         },
                         {
-                            title: 'Forgot Password',
-                            url: '/forgot-password',
+                            title: 'Swaping SOL with USDC',
+                            url: '/chat/3',
                         },
                         {
-                            title: 'OTP',
-                            url: '/otp',
+                            title: 'Buying SOL with USDC',
+                            url: '/chat/4',
                         },
                     ],
                 },
-                {
-                    title: 'Errors',
-                    icon: IconBug,
-                    items: [
-                        {
-                            title: 'Unauthorized',
-                            url: '/401',
-                            icon: IconLock,
-                        },
-                        {
-                            title: 'Forbidden',
-                            url: '/403',
-                            icon: IconUserOff,
-                        },
-                        {
-                            title: 'Not Found',
-                            url: '/404',
-                            icon: IconError404,
-                        },
-                        {
-                            title: 'Internal Server Error',
-                            url: '/500',
-                            icon: IconServerOff,
-                        },
-                        {
-                            title: 'Maintenance Error',
-                            url: '/503',
-                            icon: IconBarrierBlock,
-                        },
-                    ],
-                },
+
             ],
         },
         {
