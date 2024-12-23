4666b3410c8b3d02f220204a9882eee31611eb3a
iamnas <iamnas.eth@gmail.com>
2024-12-24 02:34:02 +0530

    update in swap

--- server/src/web3/index.ts
+++ server/src/web3/index.ts
@@ -185,12 +185,16 @@ const swapToken = async (
     const txid = await connection.sendRawTransaction(transaction.serialize(), {
       skipPreflight: true,
       maxRetries: 2,
+      preflightCommitment: 'confirmed',
     });
-    await connection.confirmTransaction({
-      blockhash: latestBlockhash.blockhash,
-      lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
-      signature: txid,
-    });
+    await connection.confirmTransaction(
+      {
+        blockhash: latestBlockhash.blockhash,
+        lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+        signature: txid,
+      },
+      'confirmed'
+    );
 
     return createResponse(Status.Success, { txHash: txid });
   } catch (error: unknown) {
