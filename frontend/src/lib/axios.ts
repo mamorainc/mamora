2968f8443a665bc83f01e4710e3c3ff29c0d8d9a
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2025-01-07 21:06:51 +0530

    prep for local host dev

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,21 +1,20 @@
-import { useAuth } from '@/stores/use-auth'
-import axios from 'axios'
+import { useAuth } from "@/stores/use-auth";
+import axios from "axios";
 
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    withCredentials: true
-})
-
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
 
 api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            useAuth.getState().logout()
-        }
-        return Promise.reject(error)
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      useAuth.getState().logout();
     }
-)
+    return Promise.reject(error);
+  },
+);
